25e882262518ef7c180871001faaed4e991bfdd9
Cao Phuoc Thanh <caophuocthanh@users.noreply.github.com>
2020-11-01 22:01:00 +0700

    Update email_thread.swift

--- email_thread.swift
+++ email_thread.swift
@@ -1,41 +1,40 @@
-
 import UIKit
 
-func groupSort(arr: [Int]) -> [[Int]] {
-    guard !arr.isEmpty else { return [] }
-    var _arr = arr
-    _arr.removeFirst()
+
+func getEmailThreads(emails: [String]) -> [[Int]] {
     
-    // get array result
+    var new_emails: [(String, String, String)] = []
     var result: [[Int]] = []
-    for element in _arr {
-        if result.contains(where: { $0[0] == element}) {
-            for (i, item) in result.enumerated() {
-                guard item[0] == element else { continue }
-                result[i][1] += 1
-            }
+    
+    for email in emails {
+        var components = email.components(separatedBy: ",")
+        let from = components[0]
+        let to = components[1]
+        components.removeFirst()
+        components.removeFirst()
+        let message = components.joined(separator: ",")
+        
+        if !message.contains("---") {
+            new_emails.append((from, to, message))
+            result.append([new_emails.count, 1])
         } else {
-            result.append([element, 1])
+            let message_components = message.components(separatedBy: "---")
+            let reply = message_components.last!
+            
+            for (i , new_email) in new_emails.enumerated() {
+                if new_email.0 == to && new_email.1 == from && new_email.2 == reply {
+                    result.append([i + 1, message_components.count])
+                }
+            }
         }
     }
-    
-    // descending by frequency
-    result = result.sorted(by: { $0[1] > $1[1]})
-    
-    // ascending by value for values with matching frequences
-    result = result.sorted(by: {
-        if $0[1] == $1[1] {
-            return $0[0] < $1[0]
-        } else {
-            return $0[1] > $1[1]
-        }
-    })
-    
     return result
 }
 
-let result = groupSort(arr: [3,7,12,3]) // [4,2,1,1,2]
-// let result = groupSort(arr: [4,2,1,2,2]) // [4,2,1,1,2]
-//let result = groupSort(arr: [3,3,1,2,1]) // [4,2,1,1,2]
-
+let result = getEmailThreads(emails: ["abc@gmail.com,x@gmail.com,hello guy",
+                                      "c@asdas.com,abc@gmail.com,askfjlasfj",
+                                      "x@gmail.com,abc@gmail.com,howareasdasd---ádhaskdjhags---ádasdasd---hello guy",
+                                      "x@gmail.com,abc@gmail.com,howareasdasd---hello guy",
+                                      "x@gmail.com,abc@gmail.com,howareasdasd---hello guy"
+])
 print(result)
