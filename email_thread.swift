75400899990ac2db1c10180b34137fb9b4e711cb
Cao Phuoc Thanh <caophuocthanh@users.noreply.github.com>
2020-11-02 14:11:08 +0700

    Update email_thread.swift

--- email_thread.swift
+++ email_thread.swift
@@ -1,30 +1,40 @@
 import UIKit
 
-
 func getEmailThreads(emails: [String]) -> [[Int]] {
     
     var new_emails: [(String, String, String)] = []
     var result: [[Int]] = []
     
     for email in emails {
+        
         var components = email.components(separatedBy: ",")
-        let from = components[0]
-        let to = components[1]
+        
+        // get sender and receiver
+        let sender = components[0]
+        let receiver = components[1]
+        
+        // remove sender and reciver from components
         components.removeFirst()
         components.removeFirst()
+        
+        // get message
         let message = components.joined(separator: ",")
         
+        // case string is not a reply
         if !message.contains("---") {
-            new_emails.append((from, to, message))
+            new_emails.append((sender, receiver, message))
             result.append([new_emails.count, 1])
-        } else {
-            let message_components = message.components(separatedBy: "---")
-            let reply = message_components.last!
-            
-            for (i , new_email) in new_emails.enumerated() {
-                if new_email.0 == to && new_email.1 == from && new_email.2 == reply {
-                    result.append([i + 1, message_components.count])
-                }
+            continue
+        }
+        
+        // case string is a reply
+        let message_components = message.components(separatedBy: "---")
+        let reply_message = message_components.last! // A reply message always at last message_components flited by "---"
+        
+        for (i , new_email) in new_emails.enumerated() {
+            if new_email.0 == receiver && new_email.1 == sender && new_email.2 == reply_message {
+                result.append([i + 1, message_components.count])
+                break
             }
         }
     }
